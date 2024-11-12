c0c545488d2d60dcc82deb71922d4f7a68339c78
ariph007 <ariprayoga007@gmail.com>
2024-11-12 19:37:48 +0700

    log: add log

--- fraud/src/main/java/com/devmentor/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/devmentor/fraud/controller/FraudController.java
@@ -3,6 +3,7 @@ package com.devmentor.fraud.controller;
 import com.devmentor.fraud.model.response.FraudCheckResponse;
 import com.devmentor.fraud.service.FraudCheckHistoryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public class FraudController {
   private final FraudCheckHistoryService fraudCheckHistoryService;
 
   @GetMapping(path = "{customerId}")
   public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
     Boolean isFraudulentCustomer = fraudCheckHistoryService.isFraudulentCustomer(customerId);
+    log.info("fraud check request for customer {}" + customerId);
     return new FraudCheckResponse(isFraudulentCustomer);
   }
 }
